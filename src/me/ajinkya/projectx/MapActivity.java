5e310f60dbe373c244e505aa305b15f64355cbf4
Ajinkya <ajinkya.parakh@ymail.com>
2014-12-14 20:47:29 +0530

    1. Getting user location using network provider 2. Added custom icon for user and cab 3. User can call now :)

--- src/me/ajinkya/projectx/MapActivity.java
+++ src/me/ajinkya/projectx/MapActivity.java
@@ -1,11 +1,12 @@
 package me.ajinkya.projectx;
 
-import android.annotation.SuppressLint;
 import android.app.Dialog;
+import android.content.Intent;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
@@ -16,146 +17,210 @@ import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.GoogleMap.OnInfoWindowClickListener;
+import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapActivity extends ActionBarActivity implements LocationListener {
+public class MapActivity extends ActionBarActivity implements LocationListener, OnInfoWindowClickListener, 
+    OnMarkerClickListener {
 
-	// Google Map
-	private GoogleMap googleMap;
+  // Google Map
+  private GoogleMap googleMap;
+  private LatLng userCurrentLocation = null;
+  private Marker userMarker;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_map);
-		// Getting Google Play availability status
-		int status = GooglePlayServicesUtil
-				.isGooglePlayServicesAvailable(getBaseContext());
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_map);
+    // Getting Google Play availability status
+    int status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getBaseContext());
 
-		// Showing status
-		if (status != ConnectionResult.SUCCESS) { // Google Play Services are
-													// not available
+    // Showing status
+    if (status != ConnectionResult.SUCCESS) { // Google Play Services are
+      // not available
 
-			int requestCode = 10;
-			Dialog dialog = GooglePlayServicesUtil.getErrorDialog(status, this,
-					requestCode);
-			dialog.show();
+      int requestCode = 10;
+      Dialog dialog = GooglePlayServicesUtil.getErrorDialog(status, this, requestCode);
+      dialog.show();
 
-		} else { // Google Play Services are available
+    } else { // Google Play Services are available
 
-			// Getting reference to the SupportMapFragment of activity_main.xml
-			SupportMapFragment fm = (SupportMapFragment) getSupportFragmentManager()
-					.findFragmentById(R.id.map);
+      initilizeMap();
+      Location location = getUserLocation();
+      if (location != null) {
+        addUserToMap(location);
+        addAutosOnMap();
+        Toast.makeText(getApplicationContext(), "Click on auto to get driver details.", Toast.LENGTH_LONG).show();
+      } 
+    }
+  }
 
-			// Getting GoogleMap object from the fragment
-			googleMap = fm.getMap();
+  /**
+   * function to load map. If map is not created it will create it for you
+   * */
+  private void initilizeMap() {
+    // Getting reference to the SupportMapFragment of activity_main.xml
+    SupportMapFragment fm =
+        (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
 
-			// Enabling MyLocation Layer of Google Map
-			googleMap.setMyLocationEnabled(true);
+    // Getting GoogleMap object from the fragment
+    googleMap = fm.getMap();
 
-			// Getting LocationManager object from System Service
-			// LOCATION_SERVICE
-			LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+    googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+    googleMap.setOnMarkerClickListener(this);
+    googleMap.setOnInfoWindowClickListener(this);
 
-			// Creating a criteria object to retrieve provider
-			Criteria criteria = new Criteria();
+    // Enabling MyLocation Layer of Google Map
+    googleMap.setMyLocationEnabled(false);
+    googleMap.getUiSettings().setMyLocationButtonEnabled(false);
+    googleMap.getUiSettings().setZoomControlsEnabled(true);
 
-			// Getting the name of the best provider
-			String provider = locationManager.getBestProvider(criteria, true);
+  }
 
-			// Getting Current Location
-			Location location = locationManager.getLastKnownLocation(provider);
+  private Location getUserLocation() {
+    // Getting LocationManager object from System Service
+    // LOCATION_SERVICE
+    LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 
-			if (location != null) {
-				onLocationChanged(location);
-			}else{
-				Location mapLocation = googleMap.getMyLocation();
-				if(mapLocation!= null){
-					onLocationChanged(mapLocation);
-				}
-			}
-			locationManager.requestLocationUpdates(provider, 20000, 0, this);
-		}
-	}
+    // Creating a criteria object to retrieve provider
+    Criteria criteria = new Criteria();
 
-	/**
-	 * function to load map. If map is not created it will create it for you
-	 * */
-	@SuppressLint("NewApi")
-	private void initilizeMap() {
-		if (googleMap == null) {
-			googleMap = ((MapFragment) getFragmentManager().findFragmentById(
-					R.id.map)).getMap();
+    // Getting the name of the best provider
+    String provider = locationManager.getBestProvider(criteria, true);
 
-			// check if map is created successfully or not
-			if (googleMap == null) {
-				Toast.makeText(getApplicationContext(),
-						"Sorry! unable to create maps", Toast.LENGTH_SHORT)
-						.show();
-			}
-		}
-	}
+    // Getting Current Location
+    Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+    // locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 15, 0, this);
+    return location;
+  }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.map, menu);
-		return true;
-	}
+  public void addUserToMap(Location location) {
+    // Getting latitude of the current location
+    double latitude = location.getLatitude();
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+    // Getting longitude of the current location
+    double longitude = location.getLongitude();
 
-	@Override
-	public void onLocationChanged(Location location) {
-		// Getting latitude of the current location
-		double latitude = location.getLatitude();
+    // Creating a LatLng object for the current location
+    userCurrentLocation = new LatLng(latitude, longitude);
 
-		// Getting longitude of the current location
-		double longitude = location.getLongitude();
-		
+    MarkerOptions markerOptions = new MarkerOptions();
+    markerOptions.position(userCurrentLocation);
+    markerOptions.title("You are here.");
+    userMarker = googleMap.addMarker(markerOptions);
+    userMarker.showInfoWindow();
+    userMarker.setIcon(BitmapDescriptorFactory.fromResource(R.drawable.user));
 
-		// Creating a LatLng object for the current location
-		LatLng latLng = new LatLng(latitude, longitude);
-		
-		googleMap.addMarker(new MarkerOptions().position(latLng));
+    // Showing the current location in Google Map
+    googleMap.moveCamera(CameraUpdateFactory.newLatLng(userCurrentLocation));
 
+    // Zoom in the Google Map
+    googleMap.animateCamera(CameraUpdateFactory.zoomTo(13));
 
-		// Showing the current location in Google Map
-		googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+  }
 
-		// Zoom in the Google Map
-		googleMap.animateCamera(CameraUpdateFactory.zoomTo(15));
-		// TODO Auto-generated method stub
+  private void addAutosOnMap() {
+    // Getting latitude of the current location
+    double latitude = userCurrentLocation.latitude;
 
-	}
+    // Getting longitude of the current location
+    double longitude = userCurrentLocation.longitude;
 
-	@Override
-	public void onProviderDisabled(String provider) {
-		// TODO Auto-generated method stub
+    addAutoToMap(latitude + 0.003, longitude);
+    addAutoToMap(latitude, longitude + 0.009);
+    addAutoToMap(latitude + 0.01, longitude + 0.01);
 
-	}
+  }
 
-	@Override
-	public void onProviderEnabled(String provider) {
-		// TODO Auto-generated method stub
+  private void addAutoToMap(double latitude, double longitude) {
+    // Creating a LatLng object for the current location
+    LatLng auto = new LatLng(latitude, longitude);
 
-	}
+    MarkerOptions markerOptions = new MarkerOptions();
+    markerOptions.position(auto);
+    markerOptions.title("Umesh, 9975176444");
+    Marker marker = googleMap.addMarker(markerOptions);
+    marker.showInfoWindow();
+    marker.setIcon(BitmapDescriptorFactory.fromResource(R.drawable.auto));
+  }
 
-	@Override
-	public void onStatusChanged(String provider, int status, Bundle extras) {
-		// TODO Auto-generated method stub
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.map, menu);
+    return true;
+  }
 
-	}
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    int id = item.getItemId();
+    if (id == R.id.action_settings) {
+      return true;
+    }
+    return super.onOptionsItemSelected(item);
+  }
+
+  @Override
+  public void onLocationChanged(Location location) {
+    // Getting latitude of the current location
+    double latitude = location.getLatitude();
+
+    // Getting longitude of the current location
+    double longitude = location.getLongitude();
+
+    // Creating a LatLng object for the current location
+    userCurrentLocation = new LatLng(latitude, longitude);
+
+    googleMap.addMarker(new MarkerOptions().position(userCurrentLocation));
+
+    // Showing the current location in Google Map
+    googleMap.moveCamera(CameraUpdateFactory.newLatLng(userCurrentLocation));
+
+    // Zoom in the Google Map
+    googleMap.animateCamera(CameraUpdateFactory.zoomTo(10));
+
+  }
+
+  @Override
+  public void onProviderDisabled(String provider) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void onProviderEnabled(String provider) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void onStatusChanged(String provider, int status, Bundle extras) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public boolean onMarkerClick(Marker marker) {
+    if (marker != userMarker) {
+      marker.showInfoWindow();
+    }
+    return false;
+  }
+
+  @Override
+  public void onInfoWindowClick(Marker marker) {
+    if (marker != userMarker) {
+      Intent callIntent = new Intent(Intent.ACTION_CALL);
+      callIntent.setData(Uri.parse("tel:9975176444"));
+      startActivity(callIntent);
+    }
+  }
 }
